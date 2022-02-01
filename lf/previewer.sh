4bfaaab9ceb648a58b1078d8e5dbf87a1e8de119
alexcoder04 <alexcoder04@protonmail.com>
2022-02-01 19:14:07 +0100

    fix lf

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -4,7 +4,7 @@ case "$XDG_SESSION_TYPE" in
   x11)
     command -v ueberzug >/dev/null \
       && "$XDG_CONFIG_HOME/lf/previewer-x11.sh" "$@" \
-      || "$XDG_CONFIG_HOME/lf/previewer-plain.sh" ;;
+      || "$XDG_CONFIG_HOME/lf/previewer-plain.sh" "$@" ;;
   *)
     "$XDG_CONFIG_HOME/lf/previewer-plain.sh" "$@" ;;
 esac
