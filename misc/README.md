4f2f9a5dd5b0373e599260823986bfac94b9d60b
alexcoder04 <alexcoder04@protonmail.com>
2022-02-06 12:29:42 +0100

    a lot of cleaning up

---
+++ misc/README.md
@@ -0,0 +1,6 @@
+
+There are two types of configs in this directory:
+
+ - tiny configs that don't deserve an own folder
+ - programs I used in the past but don't use anymore
+
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,42 +11,43 @@
 
 # edit config files
 conf(){
-	case "$1" in
-		i3) $EDITOR $DOTFILES_REPO/i3-base/config ;;
-		fish) $EDITOR $DOTFILES_REPO/fish/dynamic/config.fish ;;
-		bash) $EDITOR $DOTFILES_REPO/bash/bashrc ;;
-		zsh) $EDITOR $DOTFILES_REPO/zsh/zshrc ;;
-		vim) $EDITOR $DOTFILES_REPO/vim/init.vim ;;
-		sxhkd) $EDITOR $DOTFILES_REPO/wm-utils/sxhkdrc ;;
-		*) echo "$1 is not a valid argument" ;;
-	esac
+  case "$1" in
+    i3)    $EDITOR "$DOTFILES_REPO/i3/main.config" ;;
+    bash)  $EDITOR "$DOTFILES_REPO/bash/bashrc" ;;
+    zsh)   $EDITOR "$DOTFILES_REPO/zsh/zshrc" ;;
+    vim)   $EDITOR "$DOTFILES_REPO/vim/init.vim" ;;
+    sxhkd) $EDITOR "$DOTFILES_REPO/wm-utils/sxhkdrc" ;;
+    *) echo "$1 is not a valid argument" ;;
+  esac
 }
 
 # download and show some information that is hard to remember
 remember(){
-	case "$1" in
-		ascii)
-			curl "http://www.asciitable.com/index/asciifull.gif" \
-					> /tmp/.ascii.gif
-			nsxiv /tmp/.ascii.gif & ;;
-		raspi_pins)
-			curl "https://i1.wp.com/mbrobotics.es/blog/wp-content/uploads/2018/05/Raspberry-GPIO-Pins_B_plus.jpg" \
-					> /tmp/.raspi.jpg
-			nsxiv /tmp/.raspi.jpg & ;;
-		latex_colors)
-			curl "https://sharelatex-wiki-cdn-671420.c.cdn77.org/learn-scripts/images/d/d3/ColoursEx6.png" \
-					>/tmp/latexcolors.png
-			nsxiv /tmp/latexcolors.png & ;;
-		*) echo "invalid argument" ;;
-	esac
-	disown
+  case "$1" in
+    ascii)
+      curl "http://www.asciitable.com/index/asciifull.gif" \
+        > /tmp/.ascii.gif
+      nsxiv /tmp/.ascii.gif & ;;
+    raspi_pins)
+      curl "https://i1.wp.com/mbrobotics.es/blog/wp-content/uploads/2018/05/Raspberry-GPIO-Pins_B_plus.jpg" \
+        > /tmp/.raspi.jpg
+      nsxiv /tmp/.raspi.jpg & ;;
+    latex_colors)
+      curl "https://sharelatex-wiki-cdn-671420.c.cdn77.org/learn-scripts/images/d/d3/ColoursEx6.png" \
+        >/tmp/latexcolors.png
+      nsxiv /tmp/latexcolors.png & ;;
+    *) echo "invalid argument" ;;
+  esac
+  disown
 }
 
 # mount a luks encrypted drive
+# use:
+# `luks-mount /dev/sdX <mapper-name>` # /dev/sdX -> /media/<mapper-name>
 luks-mount(){
-    [ -z "$1" ] && exit 1
-    [ -z "$2" ] && exit 1
-    doas cryptsetup open "$1" "$2" \
-        && doas mount -v "/dev/mapper/$2" "/media/$2"
+  [ -z "$1" ] && exit 1
+  [ -z "$2" ] && exit 1
+  doas cryptsetup open "$1" "$2" \
+    && doas mount -v "/dev/mapper/$2" "/media/$2"
 }
 
